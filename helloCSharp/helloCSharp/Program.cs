7401d3cf52b3cedfb04a9af2d36ccc5a0dfcb298
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-11-12 15:49:37 -0600

    Hello world C# and Net Core

---
+++ helloCSharp/helloCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace helloCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello C# development with Net Core using Rider IDE!");
+        }
+    }
+}
