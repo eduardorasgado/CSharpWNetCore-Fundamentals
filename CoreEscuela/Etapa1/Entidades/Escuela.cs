b2e121fc506a23773320dac949882dbeda7b94e3
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-11-12 20:53:57 -0600

    Clase Escuela creada, constructor y atributos con sus propiedades en el namespace CoreEscuelaa.Entidades

---
+++ CoreEscuela/Etapa1/Entidades/Escuela.cs
@@ -0,0 +1,30 @@
+namespace CoreEscuela.Entidades
+{
+    public class Escuela
+    {
+        // atributos
+        private string nombre;
+        // propiedades de nombre
+        public string Nombre
+        {
+            get { return "Copia: " + nombre; }
+            // value es una keyword que tiene nombre por default
+            set { nombre = value.ToUpper(); }
+        }
+
+        // cuando no hay cambios en el I/O de la variable
+        // podemos hacer las propiedades con la siguiente estructura
+        private int AñoCreacion { get; set; }
+        
+        public string Pais { get; set; }
+        public string Ciudad { set; get; }
+        
+        // Constructor de la escuela
+        public Escuela(string nombre, int año)
+        {
+            this.nombre = nombre;
+            this.AñoCreacion = año;
+        }
+        
+    }
+}
---
+++ CoreEscuela/Etapa1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using CoreEscuela.Entidades;
+
+namespace Etapa1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var escuela = new Escuela("Cpp Academy", 2016);
+            
+            Console.WriteLine(escuela.Nombre);
+        }
+    }
+}
