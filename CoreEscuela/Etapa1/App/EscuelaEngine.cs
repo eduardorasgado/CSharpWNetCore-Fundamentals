2a8386bfa7f647c1f73bda87789c3420f9850f10
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-11-15 03:40:58 -0600

    GenerarAlumnosAlAzar, CargarAlumnos, CargarListaAlumnos y TestDeAleatoriedad para generar de 20 a 40 alumnos distintos por cada curso.

--- CoreEscuela/Etapa1/App/EscuelaEngine.cs
+++ CoreEscuela/Etapa1/App/EscuelaEngine.cs
@@ -29,22 +29,15 @@ namespace CoreEscuela.App
 
             CargarCursos();
             CargarAsignaturas();
-            var listaAlumni = CargarAlumnos();
-            // gaurdando la misma lista de alumnos para cada
-            // curso
-            foreach (var curso in Escuela.CursosLista)
-            {
-                // estamos agregando por primera vez
-                // Es preferible utilizar el operador de asig-
-                // nacion en lugar de AddRange
-                curso.Alumnos = listaAlumni.ToList();
-            }
+            CargarListaAlumnos();
             CargarEvaluaciones();
+
+            TestingDeAleatoriedad();
         }
 
         private void CargarEvaluaciones()
         {
-            //throw new System.NotImplementedException();
+            //
         }
 
         private void CargarAsignaturas()
@@ -59,11 +52,23 @@ namespace CoreEscuela.App
                     new Asignatura{ Nombre="Electrónica Digital" },
                     new Asignatura{ Nombre = "Lenguajs de Programación" }
                 };
+                // no podemos usar AddRange hasta que definamos el contenido
+                // de curso.Asignaturas, ya que esta por el momento está
+                // nula, debemos agregarle por primera vez con =.
                 curso.Asignaturas = listaAsignaturas;
             }
         }
 
-        private IEnumerable<Alumno> CargarAlumnos()
+        /// <summary>
+        /// Toma como parametro cantidad de alumnos a generar
+        /// hace un producto cartesiano con los arreglos de string
+        /// y selecciona solo aquellos que no han sido seleccionado antes
+        /// para devolverlos de forma mas natural sin truncar los casi 400
+        /// alumnos que la listaAlumnos produce
+        /// </summary>
+        /// <param name="cantidad"></param>
+        /// <returns></returns>
+        private List<Alumno> GenerarAlumnosAlAzar()
         {
             string[] nombres = {"Eduardo", "Mario", "Francisco", "Manuel", "Fabian", "Mariana", "Victor"};
             string[] apellido1 = {"Rasgado", "Guzman", "Olmedo", "Santiago", "Peña", "Jimenez", "Amampour", "Bartolo"};
@@ -79,7 +84,46 @@ namespace CoreEscuela.App
                 from a2 in apellido2
                 select new Alumno{Nombre = $"{n} {a1} {a2}"};
             // retornamos para asignarlo a cada uno de los cursos
-            return listaAlumnos;
+            // retorna cierta cantidad y en un orden basado en el UniqueId
+            return listaAlumnos.ToList();
+        }
+
+        /// <summary>
+        /// Carga cierto número de alumnos generado aleatoriamente
+        /// cada vez que es llamado por la funcion o método
+        /// CargarListaAlumnos
+        /// </summary>
+        /// <returns></returns>
+        private List<Alumno> CargarAlumnos()
+        {
+            // generar un numero random de alumnos
+            var randGenerator = new Random();
+            
+            var numeroDeSalto = randGenerator.Next(1, 300);
+            // de 20 a 40 por curso
+            var alumnosPorCurso = randGenerator.Next(20, 40);
+            
+            // devuelve desde [numSalto: numSalto+alumnoPorCurso]
+            var alumni = GenerarAlumnosAlAzar().Skip(numeroDeSalto).Take(alumnosPorCurso).ToList();
+            
+            return alumni;
+        }
+
+        /// <summary>
+        /// Agrega alumnos generados al azar al campo Alumnos
+        /// de la clase Curso, en la inicializacion del objeto
+        /// </summary>
+        private void CargarListaAlumnos()
+        {
+            // gaurdando la misma lista de alumnos para cada
+            // curso
+            foreach (var curso in Escuela.CursosLista)
+            {
+                // estamos agregando por primera vez
+                // Es preferible utilizar el operador de asig-
+                // nacion en lugar de AddRange
+                curso.Alumnos = CargarAlumnos();
+            }
         }
 
         private void CargarCursos()
@@ -95,6 +139,21 @@ namespace CoreEscuela.App
             };
 
             Escuela.CursosLista = listaCursos;
+        }
+
+        private void TestingDeAleatoriedad()
+        {
+            // testing de que ttodos los alumnos son generados
+            // de forma aleatoria
+            foreach (var c in Escuela.CursosLista)
+            {
+                Console.WriteLine(c.Alumnos.Count);
+                
+                foreach (var a in c.Alumnos)
+                {
+                    Console.WriteLine(a.Nombre);
+                }
+            }
         }
     }
 }
