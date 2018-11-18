97d16450220259eb7ef1f492261dc96f64e16045
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-11-18 05:31:20 -0600

    Polimorfismo: ToString personalizado para la obtencion de datos de la lista de objetos EscuelaBase p2

--- CoreEscuela/Etapa1/Entidades/Asignatura.cs
+++ CoreEscuela/Etapa1/Entidades/Asignatura.cs
@@ -5,5 +5,11 @@ namespace CoreEscuela.Entidades
     /// <summary>
     /// Un curso debe de tener asignaturas
     /// </summary>
-    public class Asignatura : EscuelaBase { }
+    public class Asignatura : EscuelaBase
+    {
+        public override string ToString()
+        {
+            return $"Nombre: {Nombre}";
+        }
+    }
 }
